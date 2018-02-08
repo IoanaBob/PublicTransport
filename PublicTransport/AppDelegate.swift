b6f735b4da08b9d5f3c28404b2188d67e81bb7ec
Ioana <ioana@Castalia-iMac.local>
2018-02-08 18:32:10 +0200

    retrieves nearest bus station list along with location

--- PublicTransport/AppDelegate.swift
+++ PublicTransport/AppDelegate.swift
@@ -22,36 +22,28 @@ class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate
         return true
     }
 
-    func applicationWillResignActive(_ application: UIApplication) {
-        
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-    }
-
     func applicationDidEnterBackground(_ application: UIApplication) {
         determineCurrentLocation()
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
         determineCurrentLocation()
+    }
+    
+    func applicationWillEnterForeground(_ application: UIApplication) {
+    }
+    
+    func applicationWillResignActive(_ application: UIApplication) {
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
     func determineCurrentLocation() {
         locationManager = CLLocationManager()
         locationManager.delegate = self as CLLocationManagerDelegate
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        locationManager.distanceFilter = 2
+        locationManager.distanceFilter = 5
         locationManager.allowsBackgroundLocationUpdates = true
         
         locationManager.requestAlwaysAuthorization()
@@ -62,42 +54,39 @@ class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate
         }
     }
     
-    //    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-    //        let userLocation:CLLocation = locations[0] as CLLocation
-    //
-    //        // Call stopUpdatingLocation() to stop listening for location updates,
-    //        // other wise this function will be called every time when user location changes.
-    //
-    //        // manager.stopUpdatingLocation()
-    //
-    //        print("user latitude = \(userLocation.coordinate.latitude)")
-    //        print("user longitude = \(userLocation.coordinate.longitude)")
-    //    }
-    //
-    //    func printLocation(location: CLLocation) {
-    //        print("user latitude = \(location.coordinate.latitude)")
-    //        print("user longitude = \(location.coordinate.longitude)")
-    //        print("==============================")
-    //    }
-    //
-    //    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error)
-    //    {
-    //        print("Error \(error)")
-    //    }
-    
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         if locations.count == 0 {
             return
         }
-        
         let lastloc = locations.last
         if lastloc == nil {
             return
         }
+        printLocation(lastloc!)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Error \(error)")
+    }
+    
+    private func printLocation(_ location: CLLocation) {
+        print("user latitude = \(location.coordinate.latitude)")
+        print("user longitude = \(location.coordinate.longitude)")
         
-        print(String(lastloc!.coordinate.latitude))
-        print(String(lastloc!.coordinate.longitude))
-        
+        BusStations.allBusStations(lat: Float(location.coordinate.latitude), long: Float(location.coordinate.longitude)) { (busStations, error) in
+            if let error = error {
+                // got an error in getting the data
+                print(error)
+                return
+            }
+            guard let busStations = busStations else {
+                print("error getting all: result is nil")
+                return
+            }
+            debugPrint(busStations.stops)
+        }
+        // BusStations.getAllBusStations(lat: Float(location.coordinate.latitude), long: Float(location.coordinate.longitude))
+        print("==============================")
     }
 }
 
